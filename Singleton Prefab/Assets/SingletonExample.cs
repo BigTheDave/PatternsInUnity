1b7028000aebd906bc19767c6737ccae7fc0fdd7
DESKTOP-QB1M2QK\David <david@canazza.co.uk>
2020-12-10 11:09:22 +0000

    Singleton Prefab Step 1

---
+++ Singleton Prefab/Assets/SingletonExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SingletonExample
+{
+    private static readonly Lazy<SingletonExample> _instance = new Lazy<SingletonExample>(() => new SingletonExample());
+
+    public static SingletonExample Instance => _instance.Value;
+    private SingletonExample()
+    {
+
+    }
+
+    public void DoMyThing()
+    {
+        Debug.Log("My Thing is Done");
+    }
+}
