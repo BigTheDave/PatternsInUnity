01f5211ad5486d48663cc1559f82b92bb771ae8d
DESKTOP-QB1M2QK\David <david@canazza.co.uk>
2021-01-21 12:31:48 +0000

    Factory Pattern and Procedural Generation

---
+++ FactoryPatternProcGen/Assets/WorldBuilderController.cs
@@ -0,0 +1,62 @@
+﻿using Assets.WorldGeneration;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorldBuilderController : MonoBehaviour
+{
+    public int Seed;
+    public int Width, Height;
+    public List<TileProbability> TilePrefabs;
+    [System.Serializable]
+    public class TileProbability
+    {
+        public GameObject Prefab;
+        public int Probability;
+    }
+    public float TileSize = 1;
+    [ContextMenu("Rebuild World")]
+    public void RebuildWorld()
+    {
+        for(int i = this.transform.childCount-1; i >= 0; i--)
+        {
+            Destroy(this.transform.GetChild(i).gameObject);
+        }
+        BuildWorld();
+    }
+    public void BuildWorld()
+    {
+        IWorldFactory factory = WorldFactory.Get(WorldFactory.WorldFactoryGeneratorType.Basic)
+            .SetSeed(Seed)
+            .GridSize(Width, Height);
+        for(int id = 0; id < TilePrefabs.Count; id++)
+        {
+            factory.CreateTileTemplate(id, TilePrefabs[id].Probability);
+        }
+        DisplayWorld(factory.BuildWorld());
+    }
+    public void AnalyseWorld(List<IWorldTile> world)
+    {
+        var typeIds = world.Select(x => x.TypeId);
+        var distinctIds = typeIds.Distinct().ToList();
+        foreach(var id in distinctIds)
+        {
+            Debug.Log($"{id} count = '{typeIds.Count(t => t == id)}'");
+        }
+    }
+    public void DisplayWorld(List<IWorldTile> world)
+    {
+        AnalyseWorld(world);
+        foreach (var tile in world)
+        {
+            var prefab = TilePrefabs[tile.TypeId].Prefab;
+            var tileInstance = Instantiate(prefab,this.transform);
+            tileInstance.transform.localPosition = new Vector2(tile.X * TileSize, tile.Y * TileSize);
+        }
+    }
+    private void OnEnable()
+    {
+        BuildWorld();
+    }
+}
---
+++ FactoryPatternProcGen/Assets/WorldGeneration/WorldFactory.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.WorldGeneration
+{
+    public interface IWorldFactory {
+        IWorldFactory SetSeed(int seed);
+        IWorldFactory GridSize(int width, int height);
+        IWorldFactory CreateTileTemplate(int id, int probabilty);
+        List<IWorldTile> BuildWorld();
+    }
+    public class WorldFactory
+    {
+        public enum WorldFactoryGeneratorType
+        {
+            Basic
+        }
+        public static IWorldFactory Get(WorldFactoryGeneratorType generatorType)
+        {
+            switch (generatorType)
+            {
+                case WorldFactoryGeneratorType.Basic:
+                    return new BasicWorldFactory();
+            }
+            return null;
+        }
+    }
+    internal class BasicWorldFactory : IWorldFactory
+    {
+        private class TileTemplate
+        {
+            public int Probability { get; set; }
+            public int Id { get; set; }
+        }
+
+        private List<TileTemplate> tileTemplates = new List<TileTemplate>();
+        private int MaximumProbabilty;
+
+        private Random mRng = new Random();
+        private int mWidth = 10;
+        private int mHeight = 10;
+
+        public IWorldFactory CreateTileTemplate(int id, int probability)
+        {
+            tileTemplates.Add(new TileTemplate()
+            {
+                Id = id,
+                Probability = probability
+            });
+            return this;
+        }
+
+        public IWorldFactory GridSize(int width, int height)
+        {
+            mWidth = width;
+            mHeight = height;
+            return this;
+        }
+
+        public List<IWorldTile> BuildWorld()
+        {
+            MaximumProbabilty = tileTemplates.Sum(tt => tt.Probability);
+            List<IWorldTile> world = new List<IWorldTile>();
+            for(int x = 0; x < mWidth; x++)
+            {
+                for(int y = 0; y < mHeight; y++)
+                {
+                    var tile = new WorldTile()
+                    {
+                        X = x,
+                        Y = y,
+                        TypeId = GetNextTypeId()
+                    };
+                    
+                    world.Add(tile);
+                }
+            }
+            return world;
+        }
+
+        /* Worked through 'test' data for GetNextTypeId()
+         [0], 10
+         [1], 5
+         [2], 7
+         total = 22
+
+            roll 12
+            [0], 12 - 10 = 2
+            [1], 2 - 5 = -3 <= return 1
+
+            roll 10
+            [0], 10 - 10 = 0
+            [1], 0 - 5 = -5 <= return 1
+
+            roll 0
+            [0], 0 - 10 = -10 <= return 0
+
+            roll 21
+            [0], 21 - 10 = 11
+            [1], 11 - 5 = 6
+            [2], 6 - 7 = -1 <= return 2
+         */
+        private int GetNextTypeId()
+        {
+            var roll = mRng.Next(0, MaximumProbabilty);
+            var originalRoll = roll;
+            foreach(var tileTemplate in tileTemplates)
+            {
+                roll -= tileTemplate.Probability;
+                if (roll < 0) return tileTemplate.Id;
+            }
+            throw new Exception($"Invalid roll '{originalRoll}'. Maximum probability = '{MaximumProbabilty}'. Actual Maximum Probability = '{tileTemplates.Sum(tt=>tt.Probability)}'");
+        }
+        public IWorldFactory SetSeed(int seed)
+        {            
+            mRng = new Random(seed);
+            return this;
+        }
+    }
+}
